a1cb6a0d331cb6a6b3af6ab66f809876decd84e5
Ayngie <angelicareutersward@gmail.com>
2023-12-20 10:27:47 +0100

    👷 build: updated base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/natural-guide-to-gestational-diabetes/',
   plugins: [react()],
 })
