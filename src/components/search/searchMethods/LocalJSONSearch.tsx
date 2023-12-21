c116286139275108add435a0c21462bb47a67f47
Ayngie <angelicareutersward@gmail.com>
2023-12-21 11:28:11 +0100

    ✨ feat: added component for search results, and state for successful data fetching

--- src/components/search/searchMethods/LocalJSONSearch.tsx
+++ src/components/search/searchMethods/LocalJSONSearch.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { SearchField } from "../searchField/SearchField";
+import { SearchResults } from "../searchResults";
 import { StyledSearchSection, StyledArticle } from "../../styles/StyledWrappers";
 import { StyledHeading, Paragraph } from "../../styles/StyledTexts";
 import { StyledLoader } from "../../styles/search/StyledLoader";
@@ -13,53 +14,55 @@ interface IJSONSearchProps {
 export const LocalJSONSearch = ({ isLoading, setIsLoading}:IJSONSearchProps) => {
   const [jsonFood, setJsonFood] = useState<string>("");
   const [jsonSearchHasBeenDone, setJsonSearchHasBeenDone] = useState<boolean>(false);
+  const [jsonSuccessFetching, setJsonSuccessFetching] = useState<boolean>(false);
   const [jsonErrorFetching, setJsonErrorFetching] = useState<boolean>(false);
   
   useEffect(() => {
 
-        //call function to get data from local JSON-file
-        async function fetchJsonFood() {
-          setJsonErrorFetching(false)
-          setJsonSearchHasBeenDone(true)
+    //call function to get data from local JSON-file
+    async function fetchJsonFood() {
+      setJsonErrorFetching(false)
+      setJsonSearchHasBeenDone(true)
+
+      try {
+        const foodData = "...await getJSONData..."
+        console.log(foodData);
+
+        setJsonSuccessFetching(true)
+        } catch (error) {
+        // console.error("This is the error from SearchFood for JSON-data search: ", error);
+        setJsonErrorFetching(true);
+        return {};
+      }  
+    }
     
-          try {
-            const foodData = "...await getJSONData..."
-            console.log(foodData);
-            } catch (error) {
-            // console.error("This is the error from SearchFood for JSON-data search: ", error);
-            setJsonErrorFetching(true);
-            return {};
-          }  
-        }
-        
-        if (jsonFood != "") {
-          console.log("Fetching food from JSON-file");
-          fetchJsonFood()
-        }
-    
-        //hide loader
-        setIsLoading(false);
-    
-        return () => {
-        };
-      }, [jsonFood, setIsLoading]);
-    
+    if (jsonFood != "") {
+      console.log("Fetching food from JSON-file");
+      fetchJsonFood()
+    }
+
+    //hide loader
+    setIsLoading(false);
+
+    return () => {
+    };
+  }, [jsonFood, setIsLoading]);
     
   return (
     <>
       <StyledHeading>Sök livsmedel</StyledHeading>
       <Paragraph>Här kan du söka fram livsmedelsdata från Livsmedelsverket för att se näringsvärden!</Paragraph>
 
-
       <StyledSearchSection>
         <SearchField setFood={setJsonFood} food={jsonFood} setIsLoading={setIsLoading}/>
 
-        {isLoading && <StyledLoader></StyledLoader>}
+        { isLoading && <StyledLoader></StyledLoader> }
 
         { jsonSearchHasBeenDone && 
           <StyledArticle> 
             <p>Du sökte på: {jsonFood}</p> 
-            { jsonErrorFetching && <p>Tyvärr kunde vi inte hämta din data just nu, försök gärna igen senare!</p>}
+            { jsonErrorFetching && <p>Tyvärr kunde vi inte hämta din data just nu, försök gärna igen senare!</p> }
+            { jsonSuccessFetching && <SearchResults></SearchResults> }
           </StyledArticle>
         }
         <Paragraph>Källa: Livsmedelsverkets livsmedelsdatabas version 2023-06-13</Paragraph>        
---
+++ src/components/search/searchResults.tsx
@@ -0,0 +1,9 @@
+export const SearchResults = () => {
+
+  return (
+    <>
+        <h3>Search Results:</h3>
+        <p>Tyvärr kunde vi inte hämta din data just nu, försök gärna igen senare!</p>
+    </>
+    );
+};
