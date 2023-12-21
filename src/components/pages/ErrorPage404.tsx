c28c3ee7defaaeae894d96756f471192fe96b9bd
Ayngie <angelicareutersward@gmail.com>
2023-12-21 09:54:27 +0100

    ♻️ refactor: updated order of imports

--- src/components/pages/ErrorPage404.tsx
+++ src/components/pages/ErrorPage404.tsx
@@ -1,6 +1,6 @@
+import { Link } from "react-router-dom";
 import { StyledFullSection, ParagraphWrapper, StyledArticle } from "../styles/StyledWrappers";
 import { StyledHeading, Paragraph, StyledLinks } from "../styles/StyledTexts";
-import { Link } from "react-router-dom";
 
 export const ErrorPage404 = () => {
   return (
