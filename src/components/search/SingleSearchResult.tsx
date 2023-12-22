064794b99d2f90b20026ab869bbcdcf76e4ef369
Ayngie <angelicareutersward@gmail.com>
2023-12-22 15:20:46 +0100

    ♻️ refactor: added article, moved h3

--- src/components/search/SingleSearchResult.tsx
+++ src/components/search/SingleSearchResult.tsx
@@ -12,20 +12,22 @@ export const SingleSearchResult = ({foodItem}: ISingleSearchResultProps) => {
         <StyledFoodItemArticle>
           <ParagraphWrapper>
             <p>Du klickade på:</p>
-            <h3>{foodItem.Livsmedelsnamn}</h3>
           </ParagraphWrapper>
-          <p>Energi (kcal): {foodItem["Energi (kcal)"]}</p>
-          <p>Energi (kJ): {foodItem["Energi (kJ)"]}</p>
-          <p>Fett, totalt (g): {foodItem["Fett, totalt (g)"]}</p>
-          <p>Protein (g): {foodItem["Protein (g)"]}</p>
-          <p>Kolhydrater, tillgängliga (g): {foodItem["Kolhydrater, tillgängliga (g)"]}</p>
-          <p>Fibrer (g): {foodItem["Fibrer (g)"]}</p>
-          <p>Sockerarter, totalt (g): {foodItem["Sockerarter, totalt (g)"]}</p>
-          <p>Monosackarider (g), totalt (g): {foodItem["Monosackarider (g)"]}</p>
-          <p>Disackarider (g): {foodItem["Disackarider (g)"]}</p>
-          <p>Tillsatt socker (g): {foodItem["Tillsatt socker (g)"]}</p>
-          <p>Fritt socker (g): {foodItem["Fritt socker (g)"]}</p>
-          <p>Fullkorn totalt (g): {foodItem["Fullkorn totalt (g)"]}</p>
+          <h3>{foodItem.Livsmedelsnamn}</h3>
+          <section>
+            <p>Energi (kcal): {foodItem["Energi (kcal)"]}</p>
+            <p>Energi (kJ): {foodItem["Energi (kJ)"]}</p>
+            <p>Fett, totalt (g): {foodItem["Fett, totalt (g)"]}</p>
+            <p>Protein (g): {foodItem["Protein (g)"]}</p>
+            <p>Kolhydrater, tillgängliga (g): {foodItem["Kolhydrater, tillgängliga (g)"]}</p>
+            <p>Fibrer (g): {foodItem["Fibrer (g)"]}</p>
+            <p>Sockerarter, totalt (g): {foodItem["Sockerarter, totalt (g)"]}</p>
+            <p>Monosackarider (g), totalt (g): {foodItem["Monosackarider (g)"]}</p>
+            <p>Disackarider (g): {foodItem["Disackarider (g)"]}</p>
+            <p>Tillsatt socker (g): {foodItem["Tillsatt socker (g)"]}</p>
+            <p>Fritt socker (g): {foodItem["Fritt socker (g)"]}</p>
+            <p>Fullkorn totalt (g): {foodItem["Fullkorn totalt (g)"]}</p>
+          </section>
         </StyledFoodItemArticle>
     </div>
     );
