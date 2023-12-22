f7f80d6e86f4f01046ca59c5e6d6f7ffe03257b7
Ayngie <angelicareutersward@gmail.com>
2023-12-22 13:28:55 +0100

    ✨ feat: mapped up searchresults to show in UI

--- src/components/search/searchResults.tsx
+++ src/components/search/searchResults.tsx
@@ -1,9 +1,36 @@
-export const SearchResults = () => {
+import { IFoodItem } from "../../models/IFoodItem";
+import { BoldText } from "../styles/StyledTexts";
+import { StyledFoodItemArticle } from "../styles/StyledWrappers";
+
+interface ISearchResultsProps {
+  foodData: IFoodItem[],
+}
+
+export const SearchResults = ({foodData}: ISearchResultsProps) => {
+// console.log("Food data recieved in SearchResults: ", foodData)
 
   return (
     <>
         <h3>Search Results:</h3>
-        <p>Tyvärr kunde vi inte hämta din data just nu, försök gärna igen senare!</p>
+        <div>
+          {foodData.map((foodItem) => (
+            <StyledFoodItemArticle key={foodItem.Livsmedelsnummer}>
+              <BoldText>Livsmedelsnamn: {foodItem.Livsmedelsnamn}</BoldText>
+              <p>Energi (kcal): {foodItem["Energi (kcal)"]}</p>
+              <p>Energi (kJ): {foodItem["Energi (kJ)"]}</p>
+              <p>Fett, totalt (g): {foodItem["Fett, totalt (g)"]}</p>
+              <p>Protein (g): {foodItem["Protein (g)"]}</p>
+              <p>Kolhydrater: {foodItem["Kolhydrater, tillgängliga (g)"]}</p>
+              <p>Fibrer (g): {foodItem["Fibrer (g)"]}</p>
+              <p>Sockerarter, totalt (g): {foodItem["Sockerarter, totalt (g)"]}</p>
+              <p>Monosackarider (g), totalt (g): {foodItem["Monosackarider (g)"]}</p>
+              <p>Disackarider (g): {foodItem["Disackarider (g)"]}</p>
+              <p>Tillsatt socker (g): {foodItem["Tillsatt socker (g)"]}</p>
+              <p>Fritt socker (g): {foodItem["Fritt socker (g)"]}</p>
+              <p>Fullkorn totalt (g): {foodItem["Fullkorn totalt (g)"]}</p>
+            </StyledFoodItemArticle>
+          ))}
+        </div>
     </>
     );
 };
