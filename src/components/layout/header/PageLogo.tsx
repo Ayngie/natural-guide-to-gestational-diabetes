30c7c4146a65c1b4b798779d2d789f53966edc61
Ayngie <angelicareutersward@gmail.com>
2023-12-29 11:51:55 +0100

    🐛 bugfix: shortened url

--- src/components/layout/header/PageLogo.tsx
+++ src/components/layout/header/PageLogo.tsx
@@ -6,7 +6,7 @@ export const PageLogo = () => {
   return (
       <Link to="/natural-guide-to-gestational-diabetes/" className="logo">     
         <StyledLogoContainer>
-            <StyledLogoImg alt="G-logotype" src={process.env.PUBLIC_URL + "../../../../public/G-logo.png"}></StyledLogoImg>  
+            <StyledLogoImg alt="G-logotype" src={process.env.PUBLIC_URL + "/G-logo.png"}></StyledLogoImg>  
             <StyledLogoText>Din naturliga guide vid graviditetsdiabetes</StyledLogoText>    
         </StyledLogoContainer>
     </Link>
