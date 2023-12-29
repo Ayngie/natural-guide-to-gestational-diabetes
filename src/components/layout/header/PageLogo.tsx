0f0be427504801331a870175630eeeec765d4096
Ayngie <angelicareutersward@gmail.com>
2023-12-29 12:03:00 +0100

    🐛 bugfix: removed types/node and reverted to previous code

--- src/components/layout/header/PageLogo.tsx
+++ src/components/layout/header/PageLogo.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import logoImg from "/G-logo.png";
 import { StyledLogoContainer, StyledLogoImg, StyledLogoText } from "../../styles/layout/header/StyledLogo";
 
 export const PageLogo = () => {
@@ -6,7 +7,7 @@ export const PageLogo = () => {
   return (
       <Link to="/natural-guide-to-gestational-diabetes/" className="logo">     
         <StyledLogoContainer>
-            <StyledLogoImg alt="G-logotype" src={process.env.PUBLIC_URL + "/G-logo.png"}></StyledLogoImg>  
+            <StyledLogoImg alt="G-logotype" src={logoImg}></StyledLogoImg>  
             <StyledLogoText>Din naturliga guide vid graviditetsdiabetes</StyledLogoText>    
         </StyledLogoContainer>
     </Link>
