cf40d61bee61049b5606a273eb398124e4a9443d
Ayngie <angelicareutersward@gmail.com>
2023-12-29 11:34:01 +0100

    🐛 bugfix: create valid http(s) URL for deployment to ghpages

--- src/components/layout/header/PageLogo.tsx
+++ src/components/layout/header/PageLogo.tsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
-import logo from "/G.png"
+// import logo from "/G.png"
 import { StyledLogoContainer, StyledLogoImg, StyledLogoText } from "../../styles/layout/header/StyledLogo";
 
 export const PageLogo = () => {
 
+  const logoImgURL = new URL("../../../../public/G-logo.png", import.meta.url).href;
+
   return (
       <Link to="/natural-guide-to-gestational-diabetes/" className="logo">     
         <StyledLogoContainer>
-            <StyledLogoImg alt="G-logotype" src={logo}></StyledLogoImg>  
+            <StyledLogoImg alt="G-logotype" src={logoImgURL}></StyledLogoImg>  
             <StyledLogoText>Din naturliga guide vid graviditetsdiabetes</StyledLogoText>    
         </StyledLogoContainer>
     </Link>
