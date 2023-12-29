db4ed8022038a5565128f89fcea8212b5fe5c44f
Ayngie <angelicareutersward@gmail.com>
2023-12-29 11:49:19 +0100

    🐛 bugfix: added types/node and process.env.PUBLIC_URL for url bugfix

--- src/components/layout/header/PageLogo.tsx
+++ src/components/layout/header/PageLogo.tsx
@@ -1,15 +1,12 @@
 import { Link } from "react-router-dom";
-// import logo from "/G.png"
 import { StyledLogoContainer, StyledLogoImg, StyledLogoText } from "../../styles/layout/header/StyledLogo";
 
 export const PageLogo = () => {
 
-  const logoImgURL = new URL("../../../../public/G-logo.png", import.meta.url).href;
-
   return (
       <Link to="/natural-guide-to-gestational-diabetes/" className="logo">     
         <StyledLogoContainer>
-            <StyledLogoImg alt="G-logotype" src={logoImgURL}></StyledLogoImg>  
+            <StyledLogoImg alt="G-logotype" src={process.env.PUBLIC_URL + "../../../../public/G-logo.png"}></StyledLogoImg>  
             <StyledLogoText>Din naturliga guide vid graviditetsdiabetes</StyledLogoText>    
         </StyledLogoContainer>
     </Link>
