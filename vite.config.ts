2b6dfba2b78895a4246f88d9108b1af78b76243c
Ayngie <angelicareutersward@gmail.com>
2024-01-03 10:04:53 +0100

    👷 build: updated server host

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/natural-guide-to-gestational-diabetes/',
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+  },
 })
