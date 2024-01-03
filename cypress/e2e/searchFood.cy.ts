97a65a0ef3f1796909e6dbf59253a9a770b7d217
Ayngie <angelicareutersward@gmail.com>
2024-01-03 16:06:52 +0100

    ✅ test: updated and added tests for <SearchFood/>

--- cypress/e2e/searchFood.cy.ts
+++ cypress/e2e/searchFood.cy.ts
@@ -1,10 +1,27 @@
 describe('<SearchFood/>', () => {
-  it('should enter input, click button, and go to search results', () => {
-    cy.visit('http://localhost:3000/natural-guide-to-gestational-diabetes/search')
+  beforeEach(() => {
+    // cy.visit('https://ayngie.github.io/natural-guide-to-gestational-diabetes/'); //deployed page
+    cy.visit('http://localhost:3000/natural-guide-to-gestational-diabetes/'); //local page
 
-    cy.get('input').type('Banan')
-    cy.get('[data-cy="search-food-btn"]').click()
-
-    cy.get('h3').should('contain', 'Sökresultat:')
+    cy.viewport(1280, 720);
+    cy.get('[data-cy="desktop-nav-search-link"]').click();
+  });
+  
+  it('should (in desktop view) be able to write input, click button, and go to search results', () => {
+    cy.get('input').type('Banan');
+    cy.get('[data-cy="search-food-btn"]').click();
+    cy.get('h3').should('contain', 'Sökresultat:');
   })
+
+  it('should also in mobile view be able to write input, click button, and go to search results', () => {
+    cy.viewport(400, 720);
+
+    cy.get('[data-cy="mobile-nav-toggle-menu"]').click();
+    cy.get('[data-cy="mobile-nav-search-link"]').click();
+
+    cy.get('input').type('Banan');
+    cy.get('[data-cy="search-food-btn"]').click();
+    cy.get('h3').should('contain', 'Sökresultat:');
+  })
+
 })
