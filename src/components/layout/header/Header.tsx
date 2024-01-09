85f6b95e4615a92db8b21350dc65bbf3fa087da0
Ayngie <angelicareutersward@gmail.com>
2024-01-09 22:18:16 +0100

    🐛 bugfix: hide content when burgermenu is open

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Burger } from "./hamburgerMenu/Burger";
 import { Menu } from "./hamburgerMenu/Menu";
 import { PageLogo } from '../header/PageLogo';
@@ -6,9 +6,17 @@ import { DesktopNavBar } from "./desktopNavBar/DesktopNavBar";
 import { HamburgerMenuWrapper } from "../../styles/layout/header/hamburgermenu/HamburgerMenuWrapper";
 import { StyledHeader } from "../../styles/layout/header/StyledHeader";
 
-export const Header = () => {
+interface IHeaderProps {
+  setBurgerMenuIsOpen: (show: boolean) => void;
+}
+
+export const Header = ( {setBurgerMenuIsOpen}:IHeaderProps) => {
   const [open, setOpen] = useState(false);
   const menuId = "main-menu";
+
+  useEffect(() => {
+      open ? setBurgerMenuIsOpen(true) : setBurgerMenuIsOpen(false);
+    }, [open, setBurgerMenuIsOpen]);
 
   return (
     <StyledHeader id="/natural-guide-to-gestational-diabetes/">
