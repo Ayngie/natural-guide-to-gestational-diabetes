c040ce13629da22ad3aea0427e19f5ebabc28fa1
Ayngie <angelicareutersward@gmail.com>
2024-01-19 17:58:26 +0100

    ✨ feat: added accessibility feature

--- src/components/LinkToTop.tsx
+++ src/components/LinkToTop.tsx
@@ -3,6 +3,6 @@ import { UpSymbol } from "./styles/StyledTexts";
 
 export const LinkToTop = () => {
   return (
-    <UpSymbol><AnchorLink href="#/natural-guide-to-gestational-diabetes/">^</AnchorLink></UpSymbol>
+    <UpSymbol><AnchorLink href="#/natural-guide-to-gestational-diabetes/" aria-label="Link to top of page">^</AnchorLink></UpSymbol>
     );
 };
