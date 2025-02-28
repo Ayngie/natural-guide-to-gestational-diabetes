fb593dff49d1b9539f0047711c670d3b39c0d542
Ayngie <angelicareutersward@gmail.com>
2025-02-28 17:07:41 +0100

    Bugfix: updated base route in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/natural-guide-to-gestational-diabetes/',
+  base: '/',
   plugins: [react()],
   server: {
     host: true,
     port: 3000,
   },
-})
+});
